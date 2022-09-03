3742f7e9f4fc27d4da0d028b9dac4e771f5cb98c
Rimma <rimma@MacBook-Air-Rimma.local>
2022-09-03 19:42:01 +0300

    added changes

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 // ЗАДАЧИ логические операторы
 
 // Проверка значения из диапазона
-
-if (age >= 14 && age <= 90);
+const ageUser = +prompt("введите возраст", "");
+if (ageUser >= 14 && ageUser <= 90);
 
 // Проверка значения вне диапазона
+const age = +prompt("введите возраст", "");
 if (!(age >= 14 && age <= 90));
 
 if (age < 14 || age > 90);
 
 // ПРОВЕРКА ЛОГИНА
 let login = prompt("Кто там?", "");
-if (login == "Admin") {
+
+if (login === "Admin") {
   console.log("");
+
   let password = prompt("пароль?", "");
-  if (password == "я главный") {
+
+  if (password === "я главный") {
     console.log("Здравствуйте");
-  } else if (password == null) {
+  } else if (!password) {
     console.log("отменено");
   } else {
     console.log("неверный пароль");
   }
-} else if (login == null) {
+} else if (!login) {
   console.log("Отменено");
 } else {
   console.log("я вас не знаю");
@@ -41,8 +45,8 @@ if (a > 0 && a < 5) {
 //  Выведите новое значение переменной на экран.
 // Проверьте работу скрипта при a, равном 5, 0, -3, 2.
 
-let f = prompt("число", "");
-if (f == 0 || f == 2) {
+let f = +prompt("число", "");
+if (!f || f === 2) {
   f += 7;
   console.log(f);
 } else {
@@ -52,7 +56,7 @@ if (f == 0 || f == 2) {
 
 let e = 5;
 
-if (e == 0 || e == 2) {
+if (!e || e === 2) {
   e += 7;
   console.log(e);
 } else {
@@ -60,35 +64,10 @@ if (e == 0 || e == 2) {
   console.log(e);
 }
 
-let b = 0;
-
-if (b == 0 || b == 2) {
-  b += 7;
-  console.log(b);
-} else {
-  b /= 10;
-  console.log(b);
-}
-
-let c = -3;
-
-if (c == 0 || c == 2) {
-  c += 7;
-  console.log(c);
-} else {
-  c /= 10;
-  console.log(c);
-}
-
-let d = 2;
-
-if (d == 0 || d == 2) {
-  d += 7;
-  console.log(d);
-} else {
-  d /= 10;
-  console.log(d);
-}
+// при let e = 5 будет 0.5;
+// при let e = 0 будет 7;
+// при let e = -3 будет -0.3;
+// при let e = 2 будет 9;
 
 // 3) Если переменная a равна или меньше 1, а переменная b больше или равна 3, то выведите сумму этих переменных,
 //  иначе выведите их разность (результат вычитания). Проверьте работу скрипта при a и b, равном 1 и 3, 0 и 6, 3 и 5.
@@ -104,38 +83,9 @@ if (h >= 1 && i >= 3) {
   console.log(h - i);
 }
 
-let k = 1;
-let l = 3;
-
-if (k >= 1 && l >= 3) {
-  k + l;
-  console.log(k + l);
-} else {
-  k - l;
-  console.log(k - l);
-}
-
-let m = 0;
-let n = 6;
-
-if (m >= 1 && n >= 3) {
-  m + n;
-  console.log(m + n);
-} else {
-  m - n;
-  console.log(m - n);
-}
-
-let o = 3;
-let p = 5;
-
-if (o >= 1 && p >= 3) {
-  o + p;
-  console.log(o + p);
-} else {
-  o - p;
-  console.log(o - p);
-}
+// при let h = 1 и i = 3 будет 4;
+// при let h = 0 и i = 6 будет -6;
+// при let h = 3 и i = 5 будет 8;
 
 // 4) Если переменная a больше 2-х и меньше 11-ти, или переменная b больше или равна 6-ти и меньше 14-ти, то выведите 'Верно',
 // в противном случае выведите 'Неверно'.
