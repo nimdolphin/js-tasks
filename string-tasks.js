b2a47c05eb9fe8370efe4b58712d814fb217c22e
Rimma <rimma@MacBook-Air-Rimma.local>
2022-11-30 13:19:53 +0300

    fixed mistakes

--- string-tasks.js
+++ string-tasks.js
@@ -1,5 +1,4 @@
 // Проверка на спам
-// важность: 5
 // Напишите функцию checkSpam(str), возвращающую true, если str содержит 'viagra' или 'XXX', а иначе false.
 
 // Функция должна быть нечувствительна к регистру:
@@ -7,30 +6,20 @@
 function checkSpam(str) {
   let lowStr = str.toLowerCase();
   let upperStr = str.toUpperCase();
-  if (lowStr.includes("viagra") || upperStr.includes("XXX")) {
-    return true;
-  } else {
-    return false;
-  }
+
+  return lowStr.includes("viagra") || upperStr.includes("XXX");
 }
 
-console.log(checkSpam("buy ViAgRA now"));
-console.log(checkSpam("free xxxxx"));
-console.log(checkSpam("innocent rabbit"));
+checkSpam("buy ViAgRA now");
+checkSpam("free xxxxx");
+checkSpam("innocent rabbit");
 
 // Сделать первый символ заглавным
-// важность: 5
 // Напишите функцию ucFirst(str), возвращающую строку str с заглавным первым символом.
 
-function ucFirst(str) {
-  if (!str) {
-    return str;
-  } else {
-    return str[0].toUpperCase() + str.slice(1);
-  }
-}
+const ucFirst = (str) => str[0].toUpperCase() + str.slice(1);
 
-console.log(ucFirst("vasy"));
+ucFirst("vasy");
 
 // Усечение строки
 // Создайте функцию truncate(str, maxlength),
@@ -38,21 +27,16 @@ console.log(ucFirst("vasy"));
 //  превосходит maxlength, заменяет конец str на "…",
 //   так, чтобы её длина стала равна maxlength.
 
-// // Результатом функции должна быть та же строка,
+// Результатом функции должна быть та же строка,
 //  если усечение не требуется, либо, если необходимо,
 //   усечённая строка.
 
-function truncate(str, maxlength) {
-  if (str.length > maxlength) {
-    return str.slice(0, maxlength - 1) + "…";
-  } else {
-    return str;
-  }
-}
+const truncate = (str, maxlength) =>
+  str.length > maxlength ? str.slice(0, maxlength - 1) + "…" : str;
 
-console.log(truncate("Вот, что мне хотелось бы сказать на эту тему:", 20));
+truncate("Вот, что мне хотелось бы сказать на эту тему:", 20);
 
-console.log(truncate("Всем привет!", 20));
+truncate("Всем привет!", 20);
 
 // Выделить число
 // Есть стоимость в виде строки "$120". То есть сначала
@@ -61,10 +45,11 @@ console.log(truncate("Всем привет!", 20));
 // будет из такой строки выделять числовое значение и возвращать его.
 
 function extractCurrencyValue(str) {
-  return +str.slice(1);
+  let newStr = str.slice(1);
+  return isFinite(newStr) ? +newStr : newStr;
 }
 
-console.log(extractCurrencyValue("$120") === 120); // true
+extractCurrencyValue("$120") === 120; // true
 
 // 0.  Напишите функцию logAllChars(str), которая принимает str в
 // качестве аргумента и выводит каждый символ строки в логи.
@@ -82,16 +67,14 @@ logAllChars("hello");
 
 const str1 = "frontent$developer$wow";
 
-const newStr1 = str1.replace(/[$]/gi, "*");
+const newStr1 = str1.replaceAll("$", "*");
 console.log(newStr1);
 
 // 2.  Напишите функцию convertStringToArray(str), которая преобразует строку в массив слов.
 
 const str2 = "I am learning frontend development";
 
-const convertStringToArray = (str2) => {
-  return str2.trim().split(" ");
-};
+const convertStringToArray = (str2) => str2.trim().split(" ");
 
 const result = convertStringToArray(str2);
 
@@ -102,32 +85,26 @@ console.log(result); // ['I', 'am', 'learning', 'frontend', 'development']
 
 const str3 = "hey, buddy";
 
-function removeSymbols(str3, length) {
-  if (str3 === String || length > 0) {
-    return str3.slice(0, length);
-  }
-}
+const removeSymbols = (str3, length) =>
+  !!str3 || !length ? str3.slice(0, length) : str3;
 
-console.log(removeSymbols(str3, 4));
+removeSymbols(str3, 4);
 
 // 4. Напишите функцию pasteDashesIntoString(str), которая принимает
 // строку str в качестве аргумента и вставляет тире (-) между словами.
 // При этом все символы строки необходимо перевести в верхний регистр.
 
 const str4 = "I love pizza";
-function pasteDashesIntoString(str4) {
-  return str4.toUpperCase().split(" ").join("-");
-}
 
-console.log(pasteDashesIntoString(str4));
+const pasteDashesIntoString = (str4) => str4.toUpperCase().split(" ").join("-");
+
+pasteDashesIntoString(str4);
 
 // 5. Напишите функцию makeStringLowerCased(str), которая
 // принимает строку str в качестве аргумента и переводит в нижний регистр.
 
 function makeStringLowerCased(str5) {
-  let newStr = "";
-  newStr = str5.toLowerCase();
-  console.log(newStr);
+  let newStr = str5.toLowerCase();
   return newStr;
 }
 
@@ -141,7 +118,8 @@ function capitaliseAllWords(str) {
     .split(" ")
     .map((item) => item[0].toUpperCase() + item.slice(1))
     .join(" ");
-  console.log(newArr);
+
+  return newArr;
 }
 
 capitaliseAllWords("i love pizza");
@@ -150,9 +128,7 @@ capitaliseAllWords("i love pizza");
 // принимает строку str в качестве аргумента и делает первую букву
 // заглавной.
 
-function makeFirstLetterUpperCased(str) {
-  console.log(str[0].toUpperCase() + str.slice(1));
-}
+const makeFirstLetterUpperCased = (str) => str[0].toUpperCase() + str.slice(1);
 
 makeFirstLetterUpperCased("i love pizza");
 
@@ -161,51 +137,23 @@ makeFirstLetterUpperCased("i love pizza");
 // противоположный. Например, если вводится «Я иЗуЧаЮ ФрОнТеНд»,
 // то на выходе должно быть «я ИзУчАю ФрОнТеНд».
 
-let str6 = prompt("Я иЗуЧаЮ ФрОнТеНд");
-
-function changeLetterRegister(str6) {
-  let newStr = "";
-
-  for (key of str6) {
-    // console.log(key);
-
-    if (key === key.toUpperCase()) {
-      console.log(key.toLowerCase());
-      newStr += key.toLowerCase();
-    } else {
-      console.log(key.toUpperCase());
-      newStr += key.toUpperCase();
-    }
-  }
-  console.log(newStr);
-  return newStr;
-}
-
-changeLetterRegister(str6);
-
-// 2 VARIANT
-
 let str7 = prompt("write Я иЗуЧаЮ ФрОнТеНд", "");
 
 function changeLetter(str7) {
   let newS = "";
   for (i = 0; i < str7.length; i++) {
     let newStr = str7[i];
-    let bigW = newStr.toUpperCase();
-    let smallW = newStr.toLowerCase();
+    let bigLetter = newStr.toUpperCase();
+    let smallLetter = newStr.toLowerCase();
 
-    if (newStr === newStr.toLowerCase()) {
-      // console.log(bigW);
-      newS += bigW;
-    } else {
-      // console.log(smallW);
-      newS += smallW;
-    }
+    let newS2 = newS === smallLetter ? bigLetter : smallLetter;
+
+    newS += newS2;
   }
   return newS;
 }
 
-console.log(changeLetter(str7));
+changeLetter(str7);
 
 // 9. Напишите функцию changeLetterRegister(str), которая принимает
 //  в качестве аргумента строку и делает в ней все символы разного
@@ -218,17 +166,9 @@ function changeLetterRegister(str5) {
   let strNew5 = "";
 
   for (i = 0; i < str5.length; i++) {
-    // console.log(str5[i]);
-
-    if (!(i % 2)) {
-      console.log(str5[i].toLowerCase());
-      strNew5 += str5[i].toLowerCase();
-    } else if (i % 2 === 1) {
-      console.log(str5[i].toUpperCase());
-      strNew5 += str5[i].toUpperCase();
-    }
+    let strN = !(i % 2) ? str5[i].toLowerCase() : str5[i].toUpperCase();
+    strNew5 += strN;
   }
-  console.log(strNew5);
   return strNew5;
 }
 
@@ -238,25 +178,8 @@ changeLetterRegister(str5);
 // нулями до указаной длины числовое значение с дополнительным знаком
 //  «+» или «-» в зависимости от передаваемого аргумента.
 
-function addZeros(num, len, sign) {
-  console.log("сейчас длина строки:", num.length);
-
-  console.log("пусть строка имеет длину:", len);
-
-  let a = num.length < len;
-  let c = "+" === sign;
-  let b = "-" === sign;
-
-  let newNum = "";
-
-  if (a && c) {
-    newNum = +(num + "0".repeat(len - num.length));
-  } else if (a && b) {
-    newNum = -(num + "0".repeat(len - num.length));
-  }
-  console.log(newNum);
-  return newNum;
-}
+const addZeros = (num, len, sign) =>
+  +(sign + (num + "0".repeat(len - num.length)));
 
 addZeros("123", 6, "-");
 
@@ -267,7 +190,7 @@ function compareStrings(str1, str2) {
   let a = str1.toLowerCase();
   let b = str2.toLowerCase();
 
-  console.log(a === b);
+  return a === b;
 }
 
 compareStrings("string", "StRiNg");
@@ -278,7 +201,7 @@ compareStrings("JavaScript", "JavaScriptonite");
 
 function repeatStr(str, n) {
   let str2 = str.repeat(n);
-  console.log(str2);
+  return str2;
 }
 
 repeatStr("hello", 5);
@@ -303,7 +226,6 @@ const pathname = "/home/user/landing/index.js";
 const getPathname = (pathname) => {
   const newPathname = pathname.split("/").slice(-1).join("");
 
-  console.log(newPathname);
   return newPathname;
 };
 
@@ -317,22 +239,13 @@ const str = "Javascript — синхронный язык программиро
 
 function getSubstring(str, char, pos) {
   let newStr = str.split(" ");
-  let newStr2 = "";
-  console.log(newStr);
+  let idx = newStr.indexOf(char);
 
-  console.log(newStr.indexOf(char));
+  let newString = pos
+    ? newStr.slice(0, idx).join(" ")
+    : newStr.slice(0, idx).join(" ");
 
-  let d = "after" === pos;
-  let f = "before" === pos;
-
-  if (newStr.includes(char) && d) {
-    newStr2 = newStr.slice(0, 1).join(" ");
-  } else if (newStr.includes(char) && f) {
-    newStr2 = newStr.slice(0, 3).join(" ");
-  }
-
-  console.log(newStr2);
-  return newStr2;
+  return newString;
 }
 
 getSubstring(str, "—", "after");
